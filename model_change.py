4a76235143fd12c6a859e89ac9c942d4ea7c213e
Jiwon Yang <zzangjiwon0225@naver.com>
2024-05-15 16:24:43 +0900

    .
    .

---
+++ model_change.py
@@ -0,0 +1,5 @@
+from keras.models import load_model
+
+#model = load_model("Clap_game/models/model.keras")
+
+#model.save("Clap_g)
--- static/js/clap_memory.js
+++ static/js/clap_memory.js
@@ -11,7 +11,7 @@ function cal_level(){
     }
 }
 
-// 랜덤 건강박수 출력
+//print clap
 const clapImgList = ["static/img/yellow_flower.png", "static/img/egg_clap.png", "static/img/wrist_clap.png", "static/img/fist_clap.png", "static/img/edge_clap.png"];
 const clapNameList = ["", "egg clap", "wrist clap", "fist clap", "edge clap"];
 const randClap = document.getElementById('sample_img');
@@ -40,7 +40,7 @@ function assign_clap_img() {
     setTimeout(repeat_assign, 1500);
 }
 
-// 페이지 로드 시 게임 시작 창 등장
+// game start
 const modal = document.querySelector('.modal');
 document.addEventListener("DOMContentLoaded", ()=>{
     modal.style.display="flex";
@@ -64,7 +64,7 @@ round_res = round + 1;
 result.textContent = round_res + "단계 달성!";
 
 const filePath = 'static/js/clap_data.txt';
-// clap_data 읽기
+// clap_data 
 const cognition1 = document.getElementById("cognition1");
 const cognition2 = document.getElementById("cognition2");
 const cognition3 = document.getElementById("cognition3");
@@ -78,7 +78,7 @@ const cognitionList = ["cognition1", "cognition2", "cognition3", "cognition4",
             "cognition5", "cognition6", "cognition7", "cognition8"];
 
 function watch_dataFile() {
-    // 페이지가 닫힐 때 setInterval을 멈추기 위한 변수
+    // setInterval stop
     var intervalId;
     function load_dataFile() {
         fetch(filePath)
@@ -90,21 +90,20 @@ function watch_dataFile() {
 
                 lines.forEach((line, index) => {
                     cognitionList.forEach((cognition, i) => {
-                        document.getElementById(cognition).textContent = lines[i] || ''; // lines 배열의 길이를 초과하는 경우 빈 문자열로 설정
+                        document.getElementById(cognition).textContent = lines[i] || ''; 
                     });
                 });
             })
             .catch(error => console.error('file load error', error));
     }
-    // 페이지가 로드될 때 setInterval 시작
-    intervalId = setInterval(load_dataFile, 500); // 0.5초마다 확인
+    intervalId = setInterval(load_dataFile, 500); // 0.5
 
-    // 페이지가 닫힐 때 clearInterval 호출하여 setInterval 중지
     window.addEventListener('unload', () => {
         clearInterval(intervalId);
     });
   };
-// 정답 버튼 누를 시
+
+//press correct button
 var correct = 0;
 const answer = document.getElementById('answer');
 const result_modal = document.querySelector('.result_modal');
@@ -126,7 +125,7 @@ answer.addEventListener("click", ()=>{
     }
 })
 
-// 다시 버튼 누를 시
+//reset button
 const reset = document.getElementById('reset');
 reset.addEventListener("click", () => {
     fetch('/reset')
