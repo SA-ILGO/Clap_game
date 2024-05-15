f29172f61d877ce43a47c5f28db8dd048631ef58
SAILGO <121847777+SA-ILGO@users.noreply.github.com>
2024-05-15 16:53:36 +0900

    0515 sejin
    app.py 수정

--- app.py
+++ app.py
@@ -35,7 +35,8 @@ def GenerateFrames():
     actions = ['egg clap', 'wrist clap', 'fist clap', 'edge clap']
     seq_length = 20
 
-    model = load_model("Clap_game\models\model2.keras")
+    #model = load_model("Clap_game\models\model.keras")
+    #model = load_model("Clap_game\models\model2.keras")
 
     # MediaPipe hands model
     mp_hands = mp.solutions.hands
@@ -149,7 +150,63 @@ def GenerateFrames():
     cv2.destroyAllWindows()
 
 def GenerateFrames2():
-    print("JIWON")
+    flag2 = 0
+    left_hand_position = 0
+    right_hand_position = 400
+
+    # MediaPipe hands model
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    hands = mp_hands.Hands(
+        max_num_hands=2,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5)
+
+    win_w, win_h = cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    while cap.isOpened():
+        ret, img = cap.read()
+        img0 = img.copy()
+
+        img = cv2.flip(img, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        result = hands.process(img)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        if result.multi_hand_landmarks is not None:
+            for res in result.multi_hand_landmarks:
+                joint = np.zeros((21, 4))
+                for j, lm in enumerate(res.landmark):
+                    joint[j] = [lm.x, lm.y, lm.z, lm.visibility]
+
+                mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+
+                wrist_x = res.landmark[0].x * img.shape[1]  
+                if wrist_x < img.shape[1] / 2:
+                    left_hand_position = wrist_x
+                else:
+                    right_hand_position = wrist_x
+
+                if flag2 == 1:
+                    if right_hand_position - left_hand_position < 150:
+                        action = "Clap!!"
+                        flag2 = 0 
+   
+                elif flag2 == 0:
+                    if right_hand_position - left_hand_position > 150:
+                        action = "Ready..."
+                        flag2 = 1
+                            
+                cv2.putText(img, f'{action.upper()}', 
+                            org=(int(win_w/2 - len(action.upper())*6), int(win_h/10)),  
+                            fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, ), thickness=2)
+
+        ret, buffer = cv2.imencode('.jpg', img)  
+        frame = buffer.tobytes()  
+        yield (b'--frame\r\n'
+                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    cv2.destroyAllWindows()
 
 @app.route('/clap_memory', methods=["GET", "POST"])
 def clap_memory():
